c8b568a357e6e8c48a9d1893d68667838e60ed15
이민욱 <lmo9903@gmail.com>
2022-11-08 01:59:44 +0900

    [remove] 불필요 파일 제거

--- recipe/src/main/java/teamproject/capstone/recipe/service/api/OpenAPIDataProvider.java
+++
@@ -1,7 +0,0 @@
-package teamproject.capstone.recipe.service.api;
-
-import org.springframework.data.domain.Page;
-
-public interface OpenAPIDataProvider {
-    Page<Object[]> allAPIDataSources();
-}
