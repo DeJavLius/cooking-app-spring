45823856c3ba73a0d550a34f0e17d5ce57b37f28
이민욱 <lmo9903@gmail.com>
2022-11-16 01:54:13 +0900

    [feat] API Search 용 Wrapper build 클래스
    builder에서 list에 추가하는 클래스

--- recipe/src/main/java/teamproject/capstone/recipe/utils/api/APISearch.java
+++ recipe/src/main/java/teamproject/capstone/recipe/utils/api/APISearch.java
@@ -1,10 +1,12 @@
 package teamproject.capstone.recipe.utils.api;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class APISearch {
     private String type;
     private String keyword;
---
+++ recipe/src/main/java/teamproject/capstone/recipe/utils/api/APISearchWrapper.java
@@ -0,0 +1,68 @@
+package teamproject.capstone.recipe.utils.api;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class APISearchWrapper {
+    private final List<APISearch> apiSearchList;
+    private final APISearch detail;
+    private final APISearch part;
+    private final APISearch seq;
+    private final APISearch way;
+
+    public static class Builder {
+        private final List<APISearch> apiSearchList = new ArrayList<>();
+        private APISearch detail = APISearch.builder().build();
+        private APISearch part = APISearch.builder().build();
+        private APISearch seq = APISearch.builder().build();
+        private APISearch way = APISearch.builder().build();
+
+        public Builder detail(String keyword) {
+            if (!keyword.isEmpty()) {
+                this.detail = APISearch.builder().type("detail").keyword(keyword).build();
+                apiSearchList.add(this.detail);
+            }
+            return this;
+        }
+
+        public Builder part(String keyword) {
+            if (!keyword.isEmpty()) {
+                this.part = APISearch.builder().type("part").keyword(keyword).build();
+                apiSearchList.add(this.part);
+            }
+            return this;
+        }
+
+        public Builder seq(String keyword) {
+            if (!keyword.equals("0")) {
+                this.seq = APISearch.builder().type("seq").keyword(keyword).build();
+                apiSearchList.add(this.seq);
+            }
+            return this;
+        }
+
+        public Builder way(String keyword) {
+            if (!keyword.isEmpty()) {
+                this.way = APISearch.builder().type("way").keyword(keyword).build();
+                apiSearchList.add(this.way);
+            }
+            return this;
+        }
+
+        public APISearchWrapper build() {
+            return new APISearchWrapper(this);
+        }
+    }
+
+    private APISearchWrapper(Builder builder) {
+        apiSearchList = builder.apiSearchList;
+        detail = builder.detail;
+        part = builder.part;
+        seq = builder.seq;
+        way = builder.way;
+    }
+
+    public List<APISearch> getApiSearchList() {
+        return this.apiSearchList;
+    }
+}
