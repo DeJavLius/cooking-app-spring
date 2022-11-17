9a0511939971ccfaea6fc4791449202a3b7a0258
이민욱 <lmo9903@gmail.com>
2022-11-18 01:36:39 +0900

    [restore] firebase login cancel
    로직 구현에 난해하거나 의미가 없을 듯하여 로그인은 oauth로 유지하기로 결정

--- recipe/src/main/java/teamproject/capstone/recipe/config/SecurityConfig.java
+++ recipe/src/main/java/teamproject/capstone/recipe/config/SecurityConfig.java
@@ -1,31 +1,22 @@
 package teamproject.capstone.recipe.config;
 
-import com.google.firebase.auth.FirebaseAuth;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import teamproject.capstone.recipe.service.login.CustomOAuthService;
-import teamproject.capstone.recipe.utils.login.FirebaseTokenFilter;
 import teamproject.capstone.recipe.utils.login.handler.OAuthFailHandler;
 import teamproject.capstone.recipe.utils.login.handler.OAuthSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-@EnableGlobalMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
 public class SecurityConfig {
-    private final FirebaseAuth firebaseAuth;
-    private final UserDetailsService userDetailsService;
+    private final CustomOAuthService customOAuthService;
 
     @Bean
     public AuthenticationSuccessHandler editAuthenticationSuccessHandler() {
@@ -38,7 +29,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain firebaseFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .headers().frameOptions().disable()
@@ -59,9 +50,11 @@ public class SecurityConfig {
                 .logoutSuccessUrl("/account")
                 .clearAuthentication(true)
                 .and()
-                .addFilterBefore(new FirebaseTokenFilter(userDetailsService, firebaseAuth), UsernamePasswordAuthenticationFilter.class)
-                .exceptionHandling()
-                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
+                .oauth2Login()
+                .successHandler(editAuthenticationSuccessHandler())
+                .failureHandler(editAuthenticationFailureHandler())
+                .userInfoEndpoint()
+                .userService(customOAuthService);
 
         return http.build();
     }
