a3c1d4a5b5b19dbb0c1710df6844871868bd7b38
이민욱 <lmo9903@gmail.com>
2022-11-21 22:06:05 +0900

    [feat] firebase 데이터 가져오기

--- recipe/src/main/java/teamproject/capstone/recipe/config/FirebaseConfig.java
+++ recipe/src/main/java/teamproject/capstone/recipe/config/FirebaseConfig.java
@@ -3,7 +3,9 @@ package teamproject.capstone.recipe.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,26 +13,59 @@ import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 
+@Slf4j
 @Configuration
 public class FirebaseConfig {
+    private final ApplicationContext applicationContext;
 
-    @Bean
-    public FirebaseApp firebaseApp() throws IOException {
-        FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(firebaseSettingFromJsonFile()))
-                .setStorageBucket("ecorecipes-5f00b.appspot.com")
-                .build();
-
-        return FirebaseApp.initializeApp(options);
+    public FirebaseConfig(final ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
     }
 
-    @Bean
-    public FirebaseAuth getFirebaseAuth() throws IOException {
-        return FirebaseAuth.getInstance(firebaseApp());
+    @PostConstruct
+    public void init() {
+        firebaseApp();
+//        try {
+//            ListUsersPage page = testsFunc();
+//            while (page != null) {
+//                for (ExportedUserRecord user : page.getValues()) {
+//                    System.out.println("User: " + user.getEmail());
+//                }
+//                page = page.getNextPage();
+//            }
+//        } catch (Exception e) {
+//            log.error("error of email found", e);
+//        }
+    }
+
+    private void firebaseApp() {
+        try {
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(this.firebaseSettingFromJsonFile()))
+                    .setStorageBucket("ecorecipes-5f00b.appspot.com")
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private FileInputStream firebaseSettingFromJsonFile() throws FileNotFoundException {
-        return new FileInputStream("src/main/resources/whyitisnotrunning.json");
+        return new FileInputStream("src/main/resources/ecorecipes-5f00b-firebase-adminsdk-c962f-26f07a6fa6.json");
+    }
+
+    private InputStream firebaseSettingFromJsonFileInput() throws IOException {
+        return this.applicationContext.getResource("classpath:whyitisnotrunning.json").getInputStream();
+    }
+
+    private UserRecord testFunc() throws FirebaseAuthException {
+        return FirebaseAuth.getInstance().getUserByEmail("lmo9903@gmail.com");
+    }
+
+    private ListUsersPage testsFunc() throws FirebaseAuthException {
+        return FirebaseAuth.getInstance().listUsers(null);
     }
 }
